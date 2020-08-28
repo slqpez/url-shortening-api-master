ffe842c1e35b40443044e9045a877b764c14d8a6
slqpez <santiago.lopez16@udea.edu.co>
2020-08-28 11:04:12 -0500

    Refactor Ui.

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,16 @@
 import "./utils/navbar.js";
-import "./modules/inputLink.js";
+import Ui from "./modules/Ui.js";
+
+const $btn = document.querySelector(".btn-input-link");
+$btn.addEventListener("click", initApp);
+
+function initApp(e) {
+  if (Ui.isEmpty()) {
+    Ui.showError("Please add a link");
+  } else {
+    Ui.isCorrect();
+    //Inicializar api para generar el short link
+  }
+
+  e.preventDefault();
+}
