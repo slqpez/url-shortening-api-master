c0b18af3b67cb3c6ef42ef2792697bc967ff07b6
slqpez <santiago.lopez16@udea.edu.co>
2020-08-28 11:04:42 -0500

    Ref Ui.

--- static/js/modules/Ui.js
+++ static/js/modules/Ui.js
@@ -1,18 +1,21 @@
 export default class Ui {
-  static get link() {
-    const $link = document.querySelector(".input-link");
-    console.log(this.isEmpty($link));
+  constructor() {
+    this.link;
+  }
+  static getLink() {
+    this.link = document.querySelector(".input-link");
+    return this.link.value;
   }
 
-  static isEmpty(input) {
-    input.addEventListener("blur", () => {
-      if (input.value === "") this.showError(input, "Please add a link");
-      else {
-        this.isCorrect(input);
-      }
-    });
+  static isEmpty() {
+    if (this.getLink() === "") {
+      return true;
+    } else {
+      return false;
+    }
   }
-  static showError(input, msg) {
+  static showError(msg) {
+    const input = document.querySelector(".input-link");
     const inputContainer = document.querySelector(".input-container");
     input.style.border = "3px solid hsl(0, 87%, 67%)";
     const msgP = document.createElement("p");
@@ -23,11 +26,10 @@ export default class Ui {
     }
   }
 
-  static isCorrect(input) {
+  static isCorrect() {
+    const input = document.querySelector(".input-link");
     const msg = document.querySelector(".error");
     if (document.querySelector(".error")) msg.remove();
     input.style.border = "2px solid green";
   }
 }
-
-Ui.link;
